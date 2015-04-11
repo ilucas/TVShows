b10d67833a3c50ca54c7c1f68a87cd937dce2f8f
lucas <lucascasteletti@gmail.com>
2015-04-11 12:51:52 -0300

    Added target to a standalone app
    Added TVShows.app target.
    Setup a concept UI.

---
+++ TVShows/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TVShows
+//
+//  Created by Lucas on 11/04/15.
+//  Copyright (c) 2015 Lucas Casteletti. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TVShows/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TVShows
+//
+//  Created by Lucas on 11/04/15.
+//  Copyright (c) 2015 Lucas Casteletti. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
