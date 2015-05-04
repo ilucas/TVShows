e776de44ec19197c47d01056b8ab7ae5b1a63dee
lucas <lucascasteletti@gmail.com>
2015-05-03 22:06:18 -0300

    New UI for Adding Shows
    Created SubscriptionWindowController for handling Shows subscriptions.

---
+++ TVShows/SubscriptionWindowController.h
@@ -0,0 +1,43 @@
+//
+//  SubscriptionWindowController.h
+//  TVShows
+//
+//  Created by Lucas on 02/05/15.
+//  Copyright (c) 2015 Lucas Casteletti. All rights reserved.
+//
+
+@import Cocoa;
+@import MagicalRecord;
+
+@interface SubscriptionWindowController : NSWindowController <NSTableViewDelegate, NSTableViewDataSource>
+
+#pragma mark - Show info
+@property (weak) IBOutlet NSBox *metadataBox;
+@property (weak) IBOutlet NSImageView *showPoster;
+@property (weak) IBOutlet NSTextField *showName;
+@property (weak) IBOutlet NSTextField *showYear;
+@property (weak) IBOutlet NSTextField *showDuration;
+@property (weak) IBOutlet NSLevelIndicator *rating;
+@property (weak) IBOutlet NSTextField *genre;
+@property (unsafe_unretained) IBOutlet NSTextView *showDescription;
+@property (weak) IBOutlet NSImageView *studioLogo;
+
+@property (weak) IBOutlet NSProgressIndicator *spinner;
+@property (weak) IBOutlet NSTextField *loadingText;
+
+#pragma mark - Settings
+@property (weak) IBOutlet NSPopUpButton *showQuality;
+@property (weak) IBOutlet NSTableView *episodesTableView;
+
+#pragma mark - Show list
+@property (weak, nonatomic) IBOutlet NSTableView *showsTableView;
+@property (strong, nonatomic) IBOutlet NSArrayController *showsArrayController;
+@property (weak, nonatomic) IBOutlet NSSearchField *SearchField;
+
+#pragma mark - Actions
+- (IBAction)reloadShowList:(id)sender;
+- (IBAction)openMoreInfoURL:(id)sender;
+- (IBAction)closeWindow:(id)sender;
+- (IBAction)subscribeToShow:(id)sender;
+
+@end
---
+++ TVShows/SubscriptionWindowController.m
@@ -0,0 +1,70 @@
+//
+//  SubscriptionWindowController.m
+//  TVShows
+//
+//  Created by Lucas on 02/05/15.
+//  Copyright (c) 2015 Lucas Casteletti. All rights reserved.
+//
+
+#import "SubscriptionWindowController.h"
+
+@interface SubscriptionWindowController ()
+
+- (void)toggleLoading:(BOOL)isLoading;
+- (BOOL)userIsSubscribedToShow:(NSString*)showName;
+
+@end
+
+@implementation SubscriptionWindowController
+
+#pragma mark - Lifecycle
+
+- (void)awakeFromNib {
+    
+}
+
+- (instancetype)init {
+    return [self initWithWindowNibName:@"SubscriptionWindowController"];
+}
+
+#pragma mark - Actions
+
+- (IBAction)reloadShowList:(id)sender {
+    
+}
+
+- (IBAction)openMoreInfoURL:(id)sender {
+    
+}
+
+- (IBAction)closeWindow:(id)sender {
+    [self close];
+}
+
+- (IBAction)subscribeToShow:(id)sender {
+    
+    // Close window after subscribe to show.
+    [self closeWindow:nil];
+}
+
+#pragma mark - NSWindowDelegate
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+}
+
+#pragma mark - NSTableViewDelegate
+
+#pragma mark - NSTableViewDataSource
+
+#pragma mark - Private
+
+- (void)toggleLoading:(BOOL)isLoading {
+    
+}
+
+- (BOOL)userIsSubscribedToShow:(NSString*)showName {
+    return false;
+}
+
+@end
